0bc2d806ec81a55a59e5c93bee03753455342258
Prajal <prajalbhattarai1627@gmail.com>
2024-08-27 21:35:48 +0545

    copy content from one file to another

---
+++ FileHandling/CopyContentFromFileToFile.java
@@ -0,0 +1,18 @@
+package FileHandling;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class CopyContentFromFileToFile {
+    public static void main (String [] args) {
+        try (FileOutputStream fout = new FileOutputStream("copy.txt"); FileInputStream fin = new FileInputStream("file.txt")) {
+            int i;
+            while( (i = fin.read())!= -1) {
+                System.out.print((char)i);
+            }
+        }
+        catch (Exception e) {
+            System.out.println("error");
+        }
+    }
+}
