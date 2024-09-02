b59f74ee935f659291f6748fb8dd7b96f476c760
Prajal <prajalbhattarai1627@gmail.com>
2024-09-02 18:25:50 +0545

    Lab 3 Ques 2

---
+++ assigments/Lab_3_Prajal/Ques_2.java
@@ -0,0 +1,18 @@
+package assigments.Lab_3_Prajal;
+import java.util.*;
+public class Ques_2 {
+        public static void main (String args[] ) {
+
+            try {
+                int [] nums = {1, 2, 3, 4};
+                System.out.println(nums[3]);
+            }
+            catch (Exception e) {
+                System.out.println("There is an error");
+            }
+            finally {
+                System.out.println("The Exception has been checked");
+            }
+
+        }
+    }
