30662b7a2a6c95fbd262888a730f14aaec729e1f
Prajal <prajalbhattarai1627@gmail.com>
2024-09-02 17:58:41 +0545

    Lab 3 Question 1

---
+++ assigments/Lab_3_Prajal/Ques_1_PackageDemo.java
@@ -0,0 +1,16 @@
+package assigments.Lab_3_Prajal;
+
+//WAP to create package of your name and inside that, make a function and compile and run this program in commandLine
+
+
+class PackageDemo {
+    public PackageDemo () {
+        System.out.println("This is Package Demo class");
+    }
+}
+
+public class Ques_1_PackageDemo {
+    public static void main(String [] args) {
+        PackageDemo pd = new PackageDemo();
+    }
+}
