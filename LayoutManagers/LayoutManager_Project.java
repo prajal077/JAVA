f3fdd638de0afe69e98a62939fbc6c2cbcd7eef3
Prajal <prajalbhattarai1627@gmail.com>
2024-09-04 21:57:55 +0545

    JButtons added

---
+++ LayoutManagers/LayoutManager_Project.java
@@ -0,0 +1,34 @@
+package LayoutManagers;
+
+import javax.swing.*;
+import java.awt.*;
+
+class Layout {
+    Layout () {
+        JFrame j = new JFrame();
+
+        j.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        JLabel accountTypeLabel = new JLabel("Account Type *");
+        JRadioButton PersonalAccount = new JRadioButton("Personal Account");
+        PersonalAccount.setBounds(150, 130, 70, 20);
+        JRadioButton BusinessAccount = new JRadioButton("Business Account");
+        BusinessAccount.setBounds(220, 130, 80, 20);
+        ButtonGroup accountTypeGroup = new ButtonGroup();
+        accountTypeGroup.add(PersonalAccount);
+        accountTypeGroup.add(BusinessAccount);
+
+
+
+
+        j.setVisible(true);
+        j.setSize(800,800);
+    }
+}
+
+public class LayoutManager_Project {
+    public static void main(String [] agrs) {
+        Layout l = new Layout();
+    }
+}
