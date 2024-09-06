8a950c53e7e9b1d7505091ab74dca2a54ac6a56a
Prajal <prajalbhattarai1627@gmail.com>
2024-09-06 22:17:31 +0545

    Ques_1 Lab_4

--- assigments/Lab_4/Ques_1.java
+++ assigments/Lab_4/Ques_1.java
@@ -5,8 +5,20 @@ package assigments.Lab_4;
     store the user input in map where animal type is the key and animal name  is the value
  */
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Ques_1 {
     public static void main(String [] agrs) {
-        
+        Scanner sc = new Scanner(System.in);
+        HashMap<String, String>map = new HashMap<>();
+        System.out.println("Enter the pets name in the format aninam_type : animal_name");
+        for(int i =0; i<5; i++) {
+            String name = sc.nextLine();
+            String [] result = name.split(":");
+            map.put(result[0], result[1]);
+        }
+
+        System.out.println(map);
     }
 }
