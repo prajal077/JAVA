3548a7b273f7184e5e2c7563fb5c6082418fa76a
Prajal <prajalbhattarai1627@gmail.com>
2024-09-28 12:39:27 +0545

    custom exception 1

--- Practise_1st_Term/custom_Exception.java
+++ Practise_1st_Term/custom_Exception.java
@@ -1,4 +1,20 @@
 package Practise_1st_Term;
-
+import java.util.*;
 public class custom_Exception {
+    public static void CheckNum (int number) throws Exception {
+        if (number > 100) {
+            throw new Exception("invalid number");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        try {
+            CheckNum(n);
+        }catch (Exception e) {
+            System.out.println("error found");
+        }
+    }
 }
