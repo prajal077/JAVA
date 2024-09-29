e84287e0751deda28528757cd721ca2f3c881c46
Prajal <prajalbhattarai1627@gmail.com>
2024-09-29 21:33:56 +0545

    palindrome

--- Practise_1st_Term/Check_Pallindrome.java
+++ Practise_1st_Term/Check_Pallindrome.java
@@ -1,4 +1,26 @@
 package Practise_1st_Term;
 
+import java.util.*;
+
 public class Check_Pallindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        int org = num;
+        int rev=0;
+
+        while (num!=0) {
+            int rem = num%10;
+            rev = rev*10+rem;
+            num = num/10;
+        }
+
+        if (rev==org) {
+            System.out.println("It is palindrome");
+        }
+        else {
+            System.out.println("It is not palindrome");
+        }
+    }
 }
