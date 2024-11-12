1889e773d5b6d71fd5bfc2fe94e5809385fa3a78
Prajal <prajalbhattarai1627@gmail.com>
2024-11-12 07:55:11 +0545

    jdbc practise

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main (String [] args) {
-        System.out.println("Hello Hello");
-    }
-}
-
-
-/*String name="Prajal";
-String friend  = "Bhattarai";  */
